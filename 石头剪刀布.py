017c41c30259045a0aebf277ced9517e5cfd5c61
Xiao2255 <1156624180@qq.com>
2026-07-31 11:21:54 +0800

    Add files via upload

--- 石头剪刀布.py
+++ 石头剪刀布.py
@@ -1,5 +1,30 @@
 import random
+import json
+def save_game_record(records):
+    with open("game_record.json", "w", encoding="utf-8") as f:
+        json.dump(
+            records,
+            f,
+            ensure_ascii=False,
+            indent=4,
+        )
+#2026.7.31,新增了用户对局记录保存，优化了菜单选项管理
+def load_game_record():
+    try:
+        with open("game_record.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def read_game_record():
+    with open("game_record.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        print(data)
+
+
 def simple_rock_paper_scissors():
+    records = load_game_record()
+
     user_score = 100
     computer_score = 100
     round_count = 0
@@ -8,12 +33,16 @@ def simple_rock_paper_scissors():
     while 0 < user_score < 200:
         round_count += 1
         print(f"\n第{round_count}轮 - 用户:{user_score} 电脑:{computer_score}")
+        user_input = (input("请出拳(1/2/3)，输入q退出: "))
+        if user_input == "q":
+                print("游戏结束！")
+                break
         try:
-            user = int(input("请出拳(1/2/3): "))
+            user = int(user_input)
             if user not in [1, 2, 3]:
                 print("请输入1、2或3！")
                 continue
-        except:
+        except ValueError:
             print("输入无效！")
             continue
         computer = random.randint(1, 3)
@@ -31,10 +60,28 @@ def simple_rock_paper_scissors():
             print("你输了！-10分")
             user_score -= 10
             computer_score += 10
+    records.append({"user_score": user_score,
+                    "computer_score": computer_score})
+    save_game_record(records)
     print(f"\n游戏结束！最终分数 - 用户:{user_score}, 电脑:{computer_score}")
     if user_score >= 200:
         print("恭喜你赢了！")
     else:
         print("很遗憾你输了。")
+
 if __name__ == "__main__":
-    simple_rock_paper_scissors()
+    while True:
+        print("A：再来一把")
+        print("B:查看历史战绩")
+        print("C:退出游戏")
+
+        choice = input("请输入选项：").upper()
+        if choice == "A":
+            simple_rock_paper_scissors()
+        elif choice == "B":
+            read_game_record()
+        elif choice == "C":
+            print("退出游戏成功！")
+            break
+        else:
+            print("请输入正确选项！")
